09c8e9104d4a5d0832cfa194e6186fec6f66a50c
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-12-22 00:03:20 +0100

    Abstract Data Type meaning

---
+++ ADT_abstract_data_type.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "An Abstract Data Type defines what operations are allowed and how the data behaves, without defining how it is stored or implemented. \n" << std::endl;
+
+    return (0);
+}
