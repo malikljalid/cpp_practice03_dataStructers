e17c221c6a4a8fb1cf179490482c6ea5fa3a7b02
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-12-22 13:15:26 +0100

    map STL built in data structer

---
+++ map_dataStructer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+
+int main(void)
+{
+    std::map <std::string, int> mpNames;
+
+    mpNames["Zakaria"] = 1337;
+    mpNames["Joshwa"]  = 42;
+    mpNames["Stone"]   = 1;
+
+    if (mpNames.find("Joshwa") != mpNames.end())
+        std::cout << mpNames.find("Joshwa")->second << std::endl;
+
+    for (std::map <std::string, int>::iterator it = mpNames.begin(); it != mpNames.end(); it++)
+    {
+        std::cout << it->first << " : " << it->second << std::endl;
+    }
+
+    for (const auto &pair : mpNames)
+    {
+        std::cout<< pair.first << " : " << pair.second << std::endl;
+    }
+
+    return (0);
+}
